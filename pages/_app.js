84a97034b4b8b171b37f0166f2a6ab3975bf4dca
Elite Programmer <tusharkumar0807@gmail.com>
2022-12-01 00:59:22 +0530

    navbar and footer ui

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,22 @@ import NProgress from "nprogress";
 import {ChakraProvider} from '@chakra-ui/react';
 
 import Layout from "../components/Layout"
+import Head from 'next/head';
 
 function MyApp({Component, pageProps}) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+
+      </Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+
+    </>
+  )
 }
 
 export default MyApp
